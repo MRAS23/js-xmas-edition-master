cd9be3601fc4bee8ab8e56862f024ad97a66c953
MRAS23 <101474625+MRAS23@users.noreply.github.com>
2022-08-03 15:50:56 -0300

    Initial commit

---
+++ tarea/tarea-clase-8.js
@@ -0,0 +1,11 @@
+/*# Tarea clase 8
+
+A las 2 tareas de la clase 6, ponerles las validaciones que consideren
+necesarias.
+
+TIP: Las edades no pueden tener decimales.
+
+validad edades, nombre, etc. con regular expression con objetos, con forEach, escribir pruebas
+pero copiando los archivos de esa clase y agregandolos al repositorio
+de la clase 8 y ahi modificarlos
+*/
